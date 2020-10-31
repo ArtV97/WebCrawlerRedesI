a4d1e790ba693513b7718f0f90e03f2f2861e4c6
ArtV97 <arthurvianna@id.uff.br>
2020-10-31 14:37:00 -0300

    parseResponse

--- webCrawler.py
+++ webCrawler.py
@@ -23,11 +23,19 @@ class webCrawler():
 	def close(self):
 		self.clientSocket.close()
 
-	def processHeader(self, stringResponse): #TO DO: melhorar processHeader
-		pos = stringResponse.find(b"<!DOCTYPE html>")#retorna a posicao onde acaba o header
-		header = stringResponse[:pos]
-		cod = int(header[9:12]) #codigo retornado pelo servidor
-		return cod, stringResponse[pos:]
+	#retorna header como dicionário e o body
+	def parseResponse(self, response):
+		pos = response.rfind(b"\r\n\r\n")
+		splited_response = response[:pos].decode("utf-8").split("\r\n")
+		first_line = splited_response[0].split(" ")
+		header = {
+					"http_version": first_line[0],
+					"cod": int(first_line[1]),
+					"cod_status": " ".join(first_line[2:])}
+		for i in range(1, len(splited_response)):
+			line = splited_response[i].split(": ")
+			header[line[0]] = line[1]
+		return header, response[pos+4:] #header, body
 
 	def receiveHtml(self):
 		parser = MyHTMLParser()
@@ -36,40 +44,41 @@ class webCrawler():
 		self.connect()
 		self.send(request)
 		response = self.receive()
-		cod, response = self.processHeader(response)
-		if cod == 200:
-			html = "" #variavel que armazenará o html
+		header, body = self.parseResponse(response)
+		if header["cod"] == 200:
 			arq = open("htmlBase.html", "w")
-			while len(response) > 0: #enquanto houver dados no response
-				parser.feed(str(response))
-				html += response.decode("utf-8")
+			while len(body) < int(header["Content-Length"]): #enquanto houver dados no body
+				parser.feed(str(body))
 				for i in range (len(parser.imgsUrl)):
 					src = parser.imgsUrl.pop()
 					if src not in self.imagesUrl: self.imagesUrl.append(src)
-				response = self.receive()
-			arq.write(html)
+				body += self.receive()
+			arq.write(body.decode("utf-8"))
 			arq.close()
 		else:
-			print("Erro: A requisição retornou código: {}".format(cod))
+			print("Erro: A requisição retornou código: {} {}".format(header["cod"], header["cod_status"]))
 		self.close()
 
 	def receiveImgs(self): #TO DO: Parser do Header para checar cod do response
+		print(self.imagesUrl)
+		self.connect()
 		for i in range(len(self.imagesUrl)):
-			self.connect()
-			imgExtension = self.imagesUrl[i][self.imagesUrl[i].find(".")+1:]
-			request = "GET /{} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(self.imagesUrl[i], self.serverName)
+			request = "GET /{} HTTP/1.1\r\nHost: {}\r\n\r\n".format(self.imagesUrl[i], self.serverName)
 			request = bytes(request, 'utf-8')
-			img = bytearray(b"")
-			arq_img = open("imagem{}.{}".format(i+1, imgExtension), "wb")
 			self.send(request)
 			response = self.receive()
+			header, body = self.parseResponse(response)
 			response = response[response.rfind(b"\r\n\r\n")+4:]
-			while len(response) > 0:
-				img += response
-				response = self.receive()
-			arq_img.write(img)
-			arq_img.close()
-			self.close()
+			if header["cod"] == 200:
+				imgExtension = self.imagesUrl[i][self.imagesUrl[i].find(".")+1:]
+				arq_img = open("imagem{}.{}".format(i+1, imgExtension), "wb")
+				while len(body) < int(header["Content-Length"]):
+					body += self.receive()
+				arq_img.write(body)
+				arq_img.close()
+			else:
+				print("Erro: A requisição da img retornou código: {} {}".format(header["cod"], header["cod_status"]))
+		self.close()
 
 if __name__ == '__main__':
 	import sys, getopt
